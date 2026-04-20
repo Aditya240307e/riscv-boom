24dd6376d329b3ea95e1bca6a92d035a5c1f9095
Aditya <apple@Adityas-MacBook-Pro.local>
2026-04-20 17:12:12 +0530

    minor fix

--- src/main/scala/v4/exu/fp-pipeline.scala
+++ src/main/scala/v4/exu/fp-pipeline.scala
@@ -154,19 +154,6 @@ class FpPipeline(implicit p: Parameters)
   // **** Register Read Stage ****
   // -------------------------------------------------------------
 
-  // Tie off the Veto/Sidecar signals for the FP Issue Unit
-  // We use DontCare for the Outputs and false.B for the Inputs
-  issue_unit.io.sidecar_dis_uop.valid := false.B
-  issue_unit.io.sidecar_dis_uop.bits := DontCare
-
-  // These are usually Decoupled/Ready signals.
-  // If the error persists, try changing := to <> DontCare
-  issue_unit.io.sidecar_reinject_0.ready := false.B
-  issue_unit.io.sidecar_reinject_1.ready := false.B
-
-  issue_unit.io.sidecar_buffer_critical := false.B
-  issue_unit.io.l1_miss := false.B
-
   // Tie off any iss_uops that aren't being used by the exe_units
   // (This handles the "uninitialized" error for units that don't have enough ExeUnits)
   for (i <- exe_units.length until issue_unit.issueWidth) {
